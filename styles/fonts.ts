acd2c874546b150ef57ed5053199bfb80831a0cb
Татьяна <isskova.tatyana@yandex.ru>
2023-12-19 15:26:43 +0300

    fix: change fonts

--- styles/fonts.ts
+++ styles/fonts.ts
@@ -1,15 +1,15 @@
-import Jost from "next/font/local";
+import { Jost } from "next/font/google";
 import Furore from "next/font/local";
 import Circe from "next/font/local";
 
 export const jost = Jost({
-  src: "/next/font/local/Jost.woff",
+  subsets: ["latin"],
   variable: "--font-jost",
   display: "swap",
 });
 
 export const furore = Furore({
-  src: "/next/font/local/Furore.woff",
+  src: "./../next/font/local/Furore.woff",
   variable: "--font-furore",
   display: "swap",
 });
@@ -17,17 +17,17 @@ export const furore = Furore({
 export const circe = Circe({
   src: [
     {
-      path: "/next/font/local/Circe/Circe.woff",
+      path: "./../next/font/local/Circe/Circe.woff",
       weight: "400",
       style: "normal",
     },
     {
-      path: "/next/font/local/Circe/Circe-Bold.woff",
+      path: "./../next/font/local/Circe/Circe-Bold.woff",
       weight: "700",
       style: "bold",
     },
     {
-      path: "/next/font/local/Circe/Circe-ExtraBold.woff",
+      path: "./../next/font/local/Circe/Circe-ExtraBold.woff",
       weight: "800",
       style: "extraBold",
     },
